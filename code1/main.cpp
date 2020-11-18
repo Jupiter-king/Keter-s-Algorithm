d386948e403f36b2d533378151d155f403edf2ef
Chang Lu <285116040@qq.com>
2020-11-18 18:49:27 +0800

    update readme

---
+++ code1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+int judge(int n) {
+    vector<int> nums;
+    int sqrt_n = sqrt(n);
+    
+}
+
+int main() {
+    int L, K;
+    cin >> L;
+    cin >> K;
+    string N;
+    if (N.length() < K) {
+        cout << 404;
+        return 0;
+    }
+
+    return 0;
+}
